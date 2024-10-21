b7736e57066a3ac29e9d8d59689e910a6eb8fee7
RyotaroOda <ryotarooda0619@gmail.com>
2024-10-21 12:26:05 +0900

    remove backend.matching.ts

--- frontend/src/services/firebase-functions-client.ts
+++ frontend/src/services/firebase-functions-client.ts
@@ -4,37 +4,25 @@ import { auth, functions } from "./firebase_f.ts"; // Firebase初期化ファイ
 import { MatchResult } from "../../../shared/types";
 
 // サーバーレス関数を使ってマッチングリクエストを送信する関数
-export const requestMatch = async (rating: number): Promise<MatchResult> => {
+export const requestMatch = async () => {
   const user = auth.currentUser;
   if (!user) {
     throw new Error("ログインしていないユーザーです。");
   }
-  // const requestMatchFunction = httpsCallable<{ rating: number }, MatchResult>(
-  //   functions,
-  //   "requestMatchFunction"
-  // );
-  // console.log("requestMatchFunction wait", requestMatchFunction);
-  // const result = await requestMatchFunction({ rating }); //FIXME:ここ！
-  // console.log("requestMatchFunction waited");
-
   const requestMatchFunction = httpsCallable(functions, "requestMatchFunction");
-  console.log("requestMatchFunction", requestMatchFunction);
-  const jaison = await requestMatchFunction({ rating })
-    .then((result) => {
-      const data = result.data;
-      console.log("data", data);
-    })
-    .catch((error) => {
-      console.log("エラー");
-      console.error(error);
-    });
-  console.log("requestMatchFunction fin");
-  const result = { roomId: "test", opponentId: "test" };
-  return result;
+  const result = await requestMatchFunction(); //FIXME:ここ！
+  console.log("requestMatchFunction waited", result);
+  const matchResult: MatchResult = {
+    roomId: "test",
+    opponentId: "test",
+    message: "test",
+  };
+
+  return matchResult;
 };
 
 // サーバーレス関数を使ってマッチングキャンセルを行う関数
-export const cancelMatch = async (): Promise<MatchResult> => {
+export const cancelMatch = async () => {
   const user = auth.currentUser;
   if (!user) {
     throw new Error("ログインしていないユーザーです。");
