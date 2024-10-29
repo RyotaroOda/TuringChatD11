0caca126ffa9b7f2d86b2aaae49e7d6ac4706632
RyotaroOda <ryotarooda0619@gmail.com>
2024-10-29 18:13:52 +0900

    firebase.json update to deploy

--- backend/dist/index_serverless.js
+++ backend/dist/index_serverless.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.calculateBattleResultFunction = exports.testFunction = exports.cancelMatchFunction = exports.requestMatchFunction = void 0;
+// backend/src/index_serverless.ts
 var matchingFunctions_1 = require("./services/functions/matchingFunctions");
 Object.defineProperty(exports, "requestMatchFunction", { enumerable: true, get: function () { return matchingFunctions_1.requestMatchFunction; } });
 Object.defineProperty(exports, "cancelMatchFunction", { enumerable: true, get: function () { return matchingFunctions_1.cancelMatchFunction; } });
--- backend/src/index_serverless.ts
+++ backend/src/index_serverless.ts
@@ -1,3 +1,4 @@
+// backend/src/index_serverless.ts
 export {
   requestMatchFunction,
   cancelMatchFunction,
